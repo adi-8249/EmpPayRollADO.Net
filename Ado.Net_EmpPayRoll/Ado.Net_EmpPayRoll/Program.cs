4ae4fadf1ed5da2f8350fc66f652abeb54f35dca
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-07-07 19:57:40 +0530

    [Anjaneya]Added:Welcome to EmpPayroll Using Ado.net

---
+++ Ado.Net_EmpPayRoll/Ado.Net_EmpPayRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ado.Net_EmpPayRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to EmpPayroll Using Ado.net!");
+        }
+    }
+}
