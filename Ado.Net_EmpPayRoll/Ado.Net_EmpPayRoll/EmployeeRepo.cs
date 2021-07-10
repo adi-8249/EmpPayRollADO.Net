0952ae194909f39b418eae02176361cfc247705f
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-07-10 23:28:22 +0530

    [Anjaneya]added:Conecting with data base through ado.net

---
+++ Ado.Net_EmpPayRoll/Ado.Net_EmpPayRoll/EmployeeRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Ado.Net_EmpPayRoll
+{
+    public class EmployeeRepo
+    {
+        public static string connectionstring = @"Data Source=DESKTOP-H386B8H\MSSQLSERVER01;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection sql = new SqlConnection(connectionstring);
+    }
+}
